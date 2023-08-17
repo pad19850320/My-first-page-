ba789c8b9350c18505945c51fcd08486d1d1941e
Andrei P85 <61545818+pad19850320@users.noreply.github.com>
2023-08-17 16:52:22 +0300

    Create lesson.py

---
+++ lesson.py
@@ -0,0 +1,38 @@
+
+def right_justify(s):
+    wsp=' '
+    s2=''
+    for i in range(70):
+        s2=s2+ wsp;
+    s2=s2+s
+    return s2
+# lesson 2
+def do_twice(f):
+    f()
+    f()
+def do_for(f1):
+    f1()
+    f1()
+    f1()
+    f1()
+def print_row():
+    print('+----',end='')
+    
+def print_row2():
+    print('|    ',end='')
+    print('|    ',end='')
+    print('|')
+    
+def printrow():
+    do_twice(print_row)
+    print('+')
+    do_for(print_row2)
+    do_twice(print_row)
+    print('+')
+    do_for(print_row2)
+    // my first commit
+    
+    
+    do_twice(print_row)
+    print('+')
+    print('commit 16/08/23')
